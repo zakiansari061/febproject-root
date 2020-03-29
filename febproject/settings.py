0037c5b801bde1ede807270f38dcc4b866926d8b
Zacknoid <61409708+zakiansari061@users.noreply.github.com>
2020-03-30 00:52:42 +0400

    Update settings.py

--- febproject/settings.py
+++ febproject/settings.py
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+**STATIC_TMP = os.path.join(BASE_DIR, 'static')**
+
 STATIC_URL = '/static/'
